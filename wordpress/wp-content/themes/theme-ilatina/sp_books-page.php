be37959dedffcdb60611e80f989a2c66e284834b
juampa <49341476+juampacamarda@users.noreply.github.com>
2021-11-07 17:09:21 -0300

    Create sp_books-page.php
    creacion template para page con layout libro

---
+++ wordpress/wp-content/themes/theme-ilatina/sp_books-page.php
@@ -0,0 +1,206 @@
+<?php
+/*
+Template Name: special-book-plantilla
+*/
+?>
+<?php get_header(posts); ?>
+	<section class="bgblanco d-flex align-items-center">
+			<div class="container my-5">
+				<div class="col-md-12 livres-title">
+					<div class="livres-title-data">
+
+						<h2>
+							<?php the_title(); ?>
+						</h2>
+						<h4>
+							<?php the_field( 'autor_de_libro' ); ?>
+						</h4>
+
+					</div>
+
+					<div class="livres-compra">
+						<?php if (get_field('texto_de_compra1')) : ?>
+							<div class="compra-buttons">
+								<p class="textofondoblanco shoptitle"><strong>Acheter</strong></p>
+								<a href="<?php the_field( 'boton_de_compra1' ); ?>" role="button" class="active btn btn-danger ml-auto"><?php the_field( 'texto_de_compra1' ); ?></a>
+								<?php if (get_field('texto_de_compra2')) : ?>
+								<a href="<?php the_field( 'boton_de_compra2' ); ?>" role="button" class="active btn btn-warning ml-auto"><?php the_field( 'texto_de_compra2' ); ?></a>
+								<?php endif?> 
+								<?php if (get_field('texto_de_compra3')) : ?>
+								<a href="<?php the_field( 'boton_de_compra3' ); ?>" role="button" class="active btn btn-primary ml-auto"><?php the_field( 'texto_de_compra3' ); ?></a>
+								<?php endif?> 
+							</div>
+						<?php endif?>
+					</div>
+				</div>
+				<div class="row justify-content-center">
+					<div class="col-12 col-md-10 col-lg-4">
+						<?php if ( get_field( 'portada_libro') ) { ?>
+							<a class="click-portada" data-toggle="modal" data-target="#portadamodal">
+								<img src="<?php the_field( 'portada_libro' ); ?>" alt="Livre" class="card-img-top img-fluid" alt="imagen" />
+							</a>
+						<?php } ?>
+					   <p class="details shoptitle ml-auto textofondoblanco">
+						   <?php the_field( 'ficha_tecnica' ); ?>
+					   </p>
+						<div class="modal fade" id="portadamodal" tabindex="-1" role="dialog" aria-labelledby="portadamodalTitle" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="portadamodalTitle"><?php the_title(); ?> - <?php the_field( 'autor_de_libro' ); ?></h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										<?php if ( get_field( 'portada_libro') ) { ?>
+											<img src="<?php the_field( 'portada_libro' ); ?>" alt="Livre" class="card-img-top img-fluid" alt="imagen" />
+										<?php } ?>
+										<p class="text-justify textofondoblanco">
+											<?php the_field( 'descripcion_corta' ); ?>
+										</p>
+										<?php if (get_field('texto_de_compra1')) : ?>
+											<div class="w-100">
+												<p class="textofondoblanco shoptitle"><strong>Acheter</strong></p>
+												<a href="<?php the_field( 'boton_de_compra1' ); ?>" role="button" class="active btn btn-danger ml-auto"><?php the_field( 'texto_de_compra1' ); ?></a>
+												<?php if (get_field('texto_de_compra2')) : ?>
+												<a href="<?php the_field( 'boton_de_compra2' ); ?>" role="button" class="active btn btn-warning ml-auto"><?php the_field( 'texto_de_compra2' ); ?></a>
+												<?php endif?> 
+												<?php if (get_field('texto_de_compra3')) : ?>
+												<a href="<?php the_field( 'boton_de_compra3' ); ?>" role="button" class="active btn btn-primary ml-auto"><?php the_field( 'texto_de_compra3' ); ?></a>
+												<?php endif?> 
+											</div>
+										<?php endif?>
+									</div>
+								</div>
+							</div>
+						</div>
+					   
+					</div>
+					<div class="col-12 col-md-10 col-lg-6">
+						<p class="text-justify textofondoblanco">
+							<?php the_field( 'descripcion_corta' ); ?>
+						</p>
+					</div>
+			
+				</div>
+			</div>
+	</section>
+	<!-- seccion prensa -->
+	<section id='prensa' class='bgnegro'>
+		<div class="container">
+    <h3 class="textofondoblanco">Presse</h3>
+			<div class="row">
+      <div class="row d-flex justify-content-center">
+					<?php while( have_rows('prensa') ): the_row(); ?>
+						<div class="col-md-6 nota">
+							<p class="txt-prensa textofondoblanco">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<?php the_sub_field('extracto'); ?>
+								<i class="fa fa-quote-right" aria-hidden="true"></i>
+							</p>
+								
+							<h4 class="medio-prensa textofondoblanco">
+								<a href="<?php the_sub_field('link_del_medio'); ?>" target="blank"><?php the_sub_field('nombre_del_medio'); ?></a>
+							</h4>
+						</div>
+					<?php endwhile; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--inicio primer bloque-->
+ <?php if (get_field('titulo_preview')) : ?>
+	
+	<section id="seccion-a" class=" bgblanco d-flex align-items-center">
+
+		<div class="container  my-5">
+			<h2 class="col-md-10"><?php the_field( 'titulo_preview' ); ?></h2>
+
+			<div class="justify-content-center">
+
+				<?php the_field( 'preview' ); ?>
+
+			</div>
+		</div>
+
+	</section>
+	
+ <?php endif?>   
+	<!--fin primer bloque-->
+	<!--segundo bloque-->
+	<?php if ( get_field( 'foto_de_autor') ) { ?>
+    <section id="seccion-b" class=" bgnegro d-flex align-items-center">
+      <div class="container my-5">
+        <h2 class="col-md-10">Auteurs</h2>
+        <div class="row justify-content-center mb-3">
+          <div class="col-md-3 col-12">
+            
+              <img src="<?php the_field( 'foto_de_autor' ); ?>" alt="Auteur" class="img-fluid rounded-circle" />
+            
+          </div>
+          <div class="col-md-7 col-12 align-self-center">
+            <h3 class=""><?php the_field( 'nombre_de_autor' ); ?></h3>
+            <p class="text-justify"><?php the_field( 'bio_del_autor' ); ?>
+            </p>
+          </div>
+        </div>
+          <?php if (get_field('nombre_de_autor_2')) : ?>
+        <div class="row justify-content-center">
+          <div class="col-md-3 col-12">
+            <?php if ( get_field( 'foto_de_autor_2') ) { ?>
+              <img src="<?php the_field( 'foto_de_autor_2' ); ?>" alt="Auteur" class="rounded-circle img-fluid" />
+            <?php } ?>
+          </div>
+          <div class="col-md-7 col-12 align-self-center">
+            <h3 class=""><?php the_field( 'nombre_de_autor_2' ); ?></h3>
+            <p class=" text-justify">
+              <?php the_field( 'bio_del_autor_2' ); ?>
+            </p>
+          </div>
+        </div>
+        <?php endif ?>
+      </div>
+    </section>
+	<?php } ?>
+	<!--fin segundo bloque-->
+	<!--empieza tercer bloque-->
+	<?php if (get_field('titulo_de_video')) : ?>
+    <section id="seccion-c" class="bgnegro d-flex align-items-center">
+      <div class="container my-5">                 
+              <h2><?php the_field( 'titulo_de_video' ); ?></h2>
+              <?php the_field( 'codigo_del_video' ); ?>
+              <p class="text-justify">
+              <?php the_field( 'video_descripcion' ); ?>
+              </p>                
+        
+      </div>
+    </section>
+	<?php endif?>
+	<!--fin tercero-->
+	<?php if( have_rows('prensa') ): ?>
+		<section id="prensa"  class="bgblanco d-flex align-items-center">
+			<div class="container">
+				<h3 class="textofondoblanco">Presse</h3>
+				<div class="row d-flex justify-content-center">
+					<?php while( have_rows('prensa') ): the_row(); ?>
+						<div class="col-md-10 nota">
+							<p class="txt-prensa textofondoblanco">
+								<i class="fa fa-quote-left" aria-hidden="true"></i>
+								<?php the_sub_field('extracto'); ?>
+								<i class="fa fa-quote-right" aria-hidden="true"></i>
+							</p>
+								
+							<h4 class="medio-prensa textofondoblanco">
+								<a href="<?php the_sub_field('link_del_medio'); ?>" target="blank"><?php the_sub_field('nombre_del_medio'); ?></a>
+							</h4>
+						</div>
+					<?php endwhile; ?>
+				</div>
+			</div>
+		</section>
+	<?php endif; ?>
+	<!--fin cuarto-->
+</div><!-- fin main -->
+</div><!-- Fin wrapper --> 
+<?php get_footer();?> 
